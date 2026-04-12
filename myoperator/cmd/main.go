91fb30462dc7ab816e8925493b7c7cd2e6b7641d
Ansible Automation <teamsmiley@gmail.com>
2026-04-12 07:51:44 -0700

    kubebuilder create webhook --group apps --version v1 --kind SimpleApp --defaulting

--- myoperator/cmd/main.go
+++ myoperator/cmd/main.go
@@ -37,6 +37,7 @@ import (
 
 	appsv1 "github.com/teamsmiley/myoperator/api/v1"
 	"github.com/teamsmiley/myoperator/internal/controller"
+	webhookv1 "github.com/teamsmiley/myoperator/internal/webhook/v1"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -184,6 +185,13 @@ func main() {
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "Failed to create controller", "controller", "SimpleApp")
 		os.Exit(1)
+	}
+	// nolint:goconst
+	if os.Getenv("ENABLE_WEBHOOKS") != "false" {
+		if err := webhookv1.SetupSimpleAppWebhookWithManager(mgr); err != nil {
+			setupLog.Error(err, "Failed to create webhook", "webhook", "SimpleApp")
+			os.Exit(1)
+		}
 	}
 	// +kubebuilder:scaffold:builder
 
---
+++ myoperator/internal/webhook/v1/simpleapp_webhook.go
@@ -0,0 +1,59 @@
+/*
+Copyright 2026.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	"context"
+
+	ctrl "sigs.k8s.io/controller-runtime"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+
+	appsv1 "github.com/teamsmiley/myoperator/api/v1"
+)
+
+// nolint:unused
+// log is for logging in this package.
+var simpleapplog = logf.Log.WithName("simpleapp-resource")
+
+// SetupSimpleAppWebhookWithManager registers the webhook for SimpleApp in the manager.
+func SetupSimpleAppWebhookWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewWebhookManagedBy(mgr, &appsv1.SimpleApp{}).
+		WithDefaulter(&SimpleAppCustomDefaulter{}).
+		Complete()
+}
+
+// TODO(user): EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+
+// +kubebuilder:webhook:path=/mutate-apps-example-com-v1-simpleapp,mutating=true,failurePolicy=fail,sideEffects=None,groups=apps.example.com,resources=simpleapps,verbs=create;update,versions=v1,name=msimpleapp-v1.kb.io,admissionReviewVersions=v1
+
+// SimpleAppCustomDefaulter struct is responsible for setting default values on the custom resource of the
+// Kind SimpleApp when those are created or updated.
+//
+// NOTE: The +kubebuilder:object:generate=false marker prevents controller-gen from generating DeepCopy methods,
+// as it is used only for temporary operations and does not need to be deeply copied.
+type SimpleAppCustomDefaulter struct {
+	// TODO(user): Add more fields as needed for defaulting
+}
+
+// Default implements webhook.CustomDefaulter so a webhook will be registered for the Kind SimpleApp.
+func (d *SimpleAppCustomDefaulter) Default(_ context.Context, obj *appsv1.SimpleApp) error {
+	simpleapplog.Info("Defaulting for SimpleApp", "name", obj.GetName())
+
+	// TODO(user): fill in your defaulting logic.
+
+	return nil
+}
